0efe428343fde398ad6547dc7d55d2184b05bbfd
Ayodeji Bankole <bankymono@gmail.com>
2024-11-03 15:02:48 +0100

    first comit

---
+++ src/main/java/com/bankymono/resourcer_server_proj/ResourcerServerProjApplication.java
@@ -0,0 +1,13 @@
+package com.bankymono.resourcer_server_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcerServerProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcerServerProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankymono/resourcer_server_proj/ResourcerServerProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankymono.resourcer_server_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcerServerProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
